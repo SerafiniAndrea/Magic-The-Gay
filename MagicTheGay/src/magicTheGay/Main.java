58139f8c740a9b0b7e303f3dadf47835c1b2793d
Andrea <serafiandri05@gmail.com>
2023-03-29 11:36:36 +0200

    Seconda prova

---
+++ MagicTheGay/src/magicTheGay/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package magicTheGay;
+
+/**
+ * @author Labmultimediale
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
